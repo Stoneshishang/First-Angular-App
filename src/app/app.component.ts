c873b941977f95de9400675287100e48cc0a206e
Shi <stoneshishang@gmail.com>
2021-11-22 09:55:29 -0500

    add external/inline html/styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  styles: [
+    `
+      h3 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = 'Shang';
