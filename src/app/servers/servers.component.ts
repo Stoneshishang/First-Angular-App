5b1d94dd3502221c9d249e62538dad514989d2b5
Shi <stoneshishang@gmail.com>
2021-11-23 17:22:18 -0500

    conditional rendering with *ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,7 +23,9 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
